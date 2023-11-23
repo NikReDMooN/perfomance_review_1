447e4a5432dabe7d61f0e5d7b58fbc68f23c0eda
Писаренко Никита Олегович <pisarenkon@ubrr.ru>
2023-11-23 14:36:09 +0300

    java_core_4_5_5

--- src/main/java/examples/java_core_4_5_5/noncapturing/example.java
+++ src/main/java/examples/java_core_4_5_5/noncapturing/example.java
@@ -1,4 +1,4 @@
-package examples.java_cor_4_5_5.capturing;
+package examples.java_core_4_5_5.noncapturing;
 
 import java.math.BigDecimal;
 import java.util.function.Function;
@@ -6,13 +6,11 @@ import java.util.function.Function;
 public class example {
 
     void a() {
-
         Function<BigDecimal, BigDecimal> func = (a) -> a.multiply(BigDecimal.TEN);
 
         for (int j = 0; j < 999999999; j++) {
             func.apply(new BigDecimal(j));
         }
-
     }
 
 }
